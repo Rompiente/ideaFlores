aa9785f14ae558ef8d4eb75c03086102a8fbcd4a
Rompiente <gmm.flores@hotmail.com>
2022-07-17 07:06:00 -0300

    Se añadió desafío 2

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div>
+
+            <button className='NavBar__button'>Menú</button>
+            <button className='NavBar__button'>Tienda</button>
+            <button className='NavBar__button'>Sobre Nosotros</button>
+            <button className='NavBar__button'>Preguntas Frecuentes</button>
+
+        </div>
+    )
+}
+
+export default NavBar
